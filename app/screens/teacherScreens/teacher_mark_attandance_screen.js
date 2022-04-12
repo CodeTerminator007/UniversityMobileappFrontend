ee413c75cb7553180c1aeeaaa924fafc96e2a6b3
Hussnain <HusnainAhmad521@gmail.com>
2022-04-12 21:00:08 +0500

    view students in attendance pannel

--- app/screens/teacherScreens/teacher_mark_attandance_screen.js
+++ app/screens/teacherScreens/teacher_mark_attandance_screen.js
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
 import { FlatList, StyleSheet, Text, View, Switch } from "react-native";
 import DatePickerr from "../../components/date_picker";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 function TeacherMarkAttandanceScreen({ route }) {
   const paasedData = route.params;
   console.log(paasedData);
 
-  const data = [
-    { name: "Ali", rollno: "11112", switch: false },
-    { name: "Ahmed", rollno: "11113", switch: false },
-    { name: "Usama", rollno: "11114", switch: false },
-    { name: "Haris", rollno: "11115", switch: false },
-    { name: "Omer", rollno: "11116", switch: false },
-    { name: "Jarar", rollno: "11117", switch: false },
-    { name: "Kaleem", rollno: "11118", switch: false },
-    { name: "Usman", rollno: "11119", switch: false },
-  ];
+  // const data = [
+  //   { name: "Ali", rollno: "11112", switch: false },
+  //   { name: "Ahmed", rollno: "11113", switch: false },
+  //   { name: "Usama", rollno: "11114", switch: false },
+  //   { name: "Haris", rollno: "11115", switch: false },
+  //   { name: "Omer", rollno: "11116", switch: false },
+  //   { name: "Jarar", rollno: "11117", switch: false },
+  //   { name: "Kaleem", rollno: "11118", switch: false },
+  //   { name: "Usman", rollno: "11119", switch: false },
+  // ];
+  const state = useSelector((state) => state);
+  const stateData = { ...state };
+  const Token = stateData.userReducer.token;
+  const [data, setdata] = useState(null);
+
+  const AuthStr = "Bearer ".concat(Token);
+  axios
+    .get(
+      `https://00c8-2400-adc7-13d-5200-abf-641e-89f1-cfde.ngrok.io/user/student/${paasedData.id}`,
+      {
+        headers: { Authorization: AuthStr },
+      }
+    )
+    .then((response) => {
+      const d = response.data;
+      const g = d.map((item) => {
+        return {
+          name: item.username,
+          rollno: item.roll_num.toString(),
+        };
+      });
+      setdata(g);
+    })
+
+    .catch((error) => {
+      console.log("error " + error);
+    });  
 
   const setSwitchValue = (val, ind) => {
     const tempData = _.cloneDeep(data);
