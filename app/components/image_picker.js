e63357b68b711a4b8a115477acd6dc3b44954e12
husnain <Husnainahmad521@gmail.com>
2022-04-18 12:13:08 +0500

    pickers

---
+++ app/components/image_picker.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  Image,
+  View,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+function ImagePickerr() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      //aspect: [4, 3],
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={pickImage}>
+        <Text style={styles.buttonText}>Select an image</Text>
+      </TouchableOpacity>
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    overflow: "hidden",
+  },
+  button: {
+    width: "80%",
+    backgroundColor: "#185079",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "white",
+  },
+});
+export default ImagePickerr;
