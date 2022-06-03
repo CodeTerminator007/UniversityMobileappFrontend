89507201dc1a4f53871f6e2036b2f994abbcdcce
Hussnain Ahmad <Husnainahmad521@gmail.com>
2022-06-03 16:38:04 +0500

    Update submitted_assignments_screen.js

--- app/screens/teacherScreens/submitted_assignments_screen.js
+++ app/screens/teacherScreens/submitted_assignments_screen.js
@@ -11,40 +11,46 @@ import AssignmentListItem from "../../components/assignment_list_item";
 import { useSelector } from "react-redux";
 import axios from "axios";
 
-function SubmittedAssignmentsScreen() {
-  data = [
-    {
-      id: 1,
-      title: "Student 1",
-      statuss: true,
-    },
-    {
-      id: 2,
-      title: "Student 2",
-      statuss: false,
-    },
-    {
-      id: 3,
-      title: "Student 3",
-      statuss: false,
-    },
-    {
-      id: 4,
-      title: "Student 4",
-      statuss: true,
-    },
-    {
-      id: 5,
-      title: "Student 5",
-      statuss: false,
-    },
-  ];
+
+
+function SubmittedAssignmentsScreen({ navigation, route }) {
+  const { id } = route.params;
+  const { class_id } = route.params;
+  const state = useSelector((state) => state);
+  const stateData = { ...state };
+  const Token = stateData.userReducer.token;
+  const AuthStr = "Bearer ".concat(Token);
+  const [data, setdata] = useState(null);  
+
+  axios
+    .get(
+      `http://d468-2400-adc7-13d-5200-aa5e-5479-6c5f-d4ed.ngrok.io/user/student/${class_id}`,
+      {
+        headers: { Authorization: AuthStr },
+      }
+    )
+    .then((response) => {
+      const d = response.data;
+      const g = d.map((item) => {
+        return {
+          id: item.id,
+          title: `${item.first_name} ${item.last_name}`,
+          class_id:class_id,
+          assignemt_id:id,
+        };
+      });
+      setdata(g);
+    })
+
+    .catch((error) => {
+      console.log("error " + error);
+    });  
 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={data}
-        keyExtractor={(data) => data.id.toString()}
+        keyExtractor={(data) => data.id}
         renderItem={({ item }) => (
           <AssignmentListItem title={item.title} statuss={item.statuss} />
         )}
