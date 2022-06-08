c5d5a399cfb994b457592a0615c4481d2706aeef
Hussnain Ahmad <Husnainahmad521@gmail.com>
2022-06-09 01:17:21 +0500

    Update add_student_screen.js

--- app/screens/adminScreens/add_student_screen.js
+++ app/screens/adminScreens/add_student_screen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback ,useSelector} from "react";
 import axios from "axios";
 import {
   StyleSheet,
