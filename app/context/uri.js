8ca12621854eb0da4acce29cca71e301666f7cfa
Hussnain Ahmad <Husnainahmad521@gmail.com>
2022-06-09 01:11:51 +0500

    different links for production and laptop

--- app/context/uri.js
+++ app/context/uri.js
@@ -1,3 +1,8 @@
 export default {
+  // for laptop branch
   uri: "http://d468-2400-adc7-13d-5200-aa5e-5479-6c5f-d4ed.ngrok.io",
+  
+  // for the production branch
+  // uri: "http://138.68.181.30:8000",
+
 };
